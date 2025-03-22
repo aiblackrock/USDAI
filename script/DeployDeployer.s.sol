9ec00e2081017d012fcf0739f18fa42b60d994de
ChunWang1998 <jjooee1998@gmail.com>
2025-03-22 12:55:48 +0800

    chore: deployer script

--- script/DeployDeployer.s.sol
+++ script/DeployDeployer.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.21;
 import {Deployer} from "src/helper/Deployer.sol";
 import {RolesAuthority, Authority} from "@solmate/auth/authorities/RolesAuthority.sol";
 import {ContractNames} from "resources/ContractNames.sol";
-import {MainnetAddresses} from "test/resources/MainnetAddresses.sol";
+import {SonicAddresses} from "test/resources/SonicAddresses.sol";
 
 import "forge-std/Script.sol";
 import "forge-std/StdJson.sol";
@@ -13,7 +13,7 @@ import "forge-std/StdJson.sol";
  *  source .env && forge script script/DeployDeployer.s.sol:DeployDeployerScript --evm-version london --broadcast --etherscan-api-key $BSCSCAN_KEY --verify
  * @dev Optionally can change `--with-gas-price` to something more reasonable
  */
-contract DeployDeployerScript is Script, ContractNames, MainnetAddresses {
+contract DeployDeployerScript is Script, ContractNames, SonicAddresses {
     uint256 public privateKey;
 
     // Contracts to deploy
@@ -23,17 +23,18 @@ contract DeployDeployerScript is Script, ContractNames, MainnetAddresses {
     uint8 public DEPLOYER_ROLE = 1;
 
     function setUp() external {
-        privateKey = vm.envUint("ETHERFI_LIQUID_DEPLOYER");
-        vm.createSelectFork("bsc");
+        privateKey = vm.envUint("PRIVATE_KEY");
+        vm.createSelectFork("sonic_testnet");
     }
 
     function run() external {
         bytes memory creationCode;
         bytes memory constructorArgs;
+ 
         vm.startBroadcast(privateKey);
 
         deployer = new Deployer(dev0Address, Authority(address(0)));
-        require(address(deployer) == 0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d, "Deployer deployment failed");
+        // require(address(deployer) == 0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d, string(abi.encodePacked("Deployer deployment failed. Actual address: ", vm.toString(address(deployer)))));
         creationCode = type(RolesAuthority).creationCode;
         constructorArgs = abi.encode(dev0Address, Authority(address(0)));
         rolesAuthority =
