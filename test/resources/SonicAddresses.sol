0e00aba86dae63fc501d2022c43120dbbe6f6a46
ChunWang1998 <jjooee1998@gmail.com>
2025-03-22 12:55:17 +0800

    chore: sonic env config

---
+++ test/resources/SonicAddresses.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+contract SonicAddresses {
+    // Liquid Ecosystem
+    address public deployerAddress = 0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d;
+    address public dev0Address = 0x8Ab8aEEf444AeE718A275a8325795FE90CF162c4;
+    address public dev1Address = 0x8Ab8aEEf444AeE718A275a8325795FE90CF162c4;
+    address public liquidV1PriceRouter = 0x693799805B502264f9365440B93C113D86a4fFF5;
+    address public liquidPayoutAddress = 0xA9962a5BfBea6918E958DeE0647E99fD7863b95A;
+    address public liquidMultisig = 0xCEA8039076E35a825854c5C2f85659430b06ec96;
+}
